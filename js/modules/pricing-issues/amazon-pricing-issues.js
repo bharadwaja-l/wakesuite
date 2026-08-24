1b01e5872feef1217d5516cbf6230db705de5835
bharadwaja-l <bharadwaja.l@wakefit.co>
2026-08-24 14:12:05 +0530

    Delete js directory

--- js/modules/pricing-issues/amazon-pricing-issues.js
+++
@@ -1,5 +0,0 @@
-/* Amazon Min/Max Pricing Issues are separate from general Pricing Exceptions. */
-(function(){
-  window.WakeSuiteAmazonPricingIssues={manualTreatment:'No Pricing Issue',generalPricingExceptions:false,minRule:'Listing Price × (1 − configured reduction %)',maxRule:'WF MRP'};
-  window.WakeSuiteModules?.register('amazonPricingIssues',window.WakeSuiteAmazonPricingIssues);
-})();
