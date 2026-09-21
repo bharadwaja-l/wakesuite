978c2711081817030854fe78bfc0ef8f7b4a4f6b
bharadwaja-l <bharadwaja.l@wakefit.co>
2026-09-21 17:28:31 +0530

    Delete js directory

--- js/modules/insights/marketplace-insights.js
+++
@@ -1,6 +0,0 @@
-/* Marketplace Insights remains issue-focused; Pricing/Inventory live under Business Insights. */
-(function(){
-  const allowed=['all','parity','price_disparity','amazon_suppression','buy_box','exceptions','revenue_impact'];
-  window.WakeSuiteMarketplaceInsights={allowedFocus:allowed};
-  window.WakeSuiteModules?.register('marketplaceInsights',window.WakeSuiteMarketplaceInsights);
-})();
